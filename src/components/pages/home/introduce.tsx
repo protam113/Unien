b2cd374ee7be3ced1cc069018198068c27dc7bbd
protam113 <64359566+protam113@users.noreply.github.com>
2025-05-02 14:02:05 +0700

    frist update

--- src/components/pages/home/introduce.tsx
+++ src/components/pages/home/introduce.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { ArrowRight, Star } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import SectionHeader from '@/components/container/SectionHeader';
 
 export default function IndustrialIntro() {
